22ff33e36d958f047fdcdbe89b77dc031f7100e4
Striatp <sebastien.works@outlook.com>
2025-11-15 14:52:12 +0100

    feat: add a program that prints 'Hello, world!' ✨

---
+++ exercices/hello_world.asm
@@ -0,0 +1,15 @@
+section .data
+    hello_msg db "Hello, world!", 13, 10, 0
+
+section .text
+global _start
+
+extern printf
+
+_start:
+    lea rcx, [rel hello_msg]
+    xor rax, rax
+    call printf
+
+    xor rax, rax
+    ret
