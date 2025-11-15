12f617f01b0ffbeb6071d6c42a35de5c8f73d2d3
Striatp <sebastien.works@outlook.com>
2025-11-15 15:59:28 +0100

    feat: add a program that prints the substraction of two numbers ✨

---
+++ exercices/sub_two_numbers.asm
@@ -0,0 +1,30 @@
+; Task: Subtract one integer from another and print the resulting value.
+
+section .data
+    fmt db "%d", 13, 10, 0
+
+section .text
+global _start
+
+extern printf
+
+_start:
+    mov rcx, 0x2
+    mov rdx, 0x5
+    call _sub_two_numbers
+
+    lea rcx, [rel fmt]
+    mov rdx, rax
+    xor rax, rax
+
+    sub rsp, 32
+    call printf
+    add rsp, 32
+
+    xor rax, rax
+    ret
+
+_sub_two_numbers:
+    mov rax, rcx
+    sub rax, rdx
+    ret
