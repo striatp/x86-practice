4874ffc66fdc1235d67f9bed015c97aca104c973
Striatp <sebastien.works@outlook.com>
2025-11-18 21:08:10 +0100

    feat: add a program that prints the sum of N numbers ✨

---
+++ exercices/sum_of_first_n.asm
@@ -0,0 +1,36 @@
+; Task: Use a loop to calculate and print the sum of the first N positive integers.
+
+section .data
+    fmt db "%d", 13, 10, 0
+
+section .text
+global _start
+
+extern printf
+
+_start:
+    xor rax, rax
+    mov rbx, 0x5
+    xor rcx, rcx
+
+.loop_start:
+    cmp rcx, rbx
+    jge .end
+
+    add rax, rcx
+    inc rcx
+
+    jmp .loop_start
+
+.end:
+    ; Print the sum
+    lea rcx, [rel fmt]
+    mov rdx, rax
+    xor rax, rax
+
+    sub rsp, 0x20
+    call printf
+    add rsp, 0x20
+
+    xor rax, rax
+    ret
