e8ce1e0b3b8a40fd1f6f9460037d6042ffa3f721
Striatp <sebastien.works@outlook.com>
2025-11-21 11:01:13 +0100

    feat: add a program that prints the second element of an array ✨

---
+++ exercices/second_element_array.asm
@@ -0,0 +1,23 @@
+section .data
+    fmt   db "%d", 13, 10, 0
+    array dq 10, 20, 30, 40
+
+section .text
+global _start
+
+extern printf
+extern ExitProcess
+
+_start:
+    mov rcx, 1
+    lea rax, [rel array + rcx*8]
+
+    sub rsp, 0x28
+    lea rcx, [rel fmt]
+    mov rdx, [rax]
+    xor rax, rax
+    call printf
+    add rsp, 0x28
+
+    xor rcx, rcx
+    call ExitProcess
